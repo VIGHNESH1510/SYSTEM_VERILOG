3c8dfea83230e3317ab46350a43fd4e31fe87f46
Vighnesh E S <Vighnesh1510@gmail.com>
2026-03-27 17:32:36 +0530

    Create 02_union_packed.v

---
+++ 01_DATATYPES/05_UNION/02_union_packed.v
@@ -0,0 +1,22 @@
+module union_tb; 
+  union packed{   // datatype & size should be same
+    bit[8:0] id;
+    bit[8:0] addr;
+  } un_var;
+  
+  initial
+    begin
+      $display("un_var = %p",un_var);
+      un_var.id = 12;
+      $display("un_var = %p",un_var);
+      un_var.addr =10;
+      $display("un_var = %p",un_var);
+    end
+endmodule
+
+/*
+OUTPUT
+# un_var = '{id:0, addr:0}
+# un_var = '{id:12, addr:12}
+# un_var = '{id:10, addr:10}
+*/
