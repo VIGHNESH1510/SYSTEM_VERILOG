0064de96b6e336da69197dbf1c6b3a60d0b7acd4
Vighnesh E S <Vighnesh1510@gmail.com>
2026-04-25 23:17:50 +0530

    Create 02_Class_Handle.sv

---
+++ 10_CLASS/02_Class_Handle.sv
@@ -0,0 +1,46 @@
+class counter;
+  int max; // variables  are called class properties
+  int count;
+  
+  function void tick(); // functions or task are called as class methods
+    if(count == max)
+      count =0;
+    else 
+      count ++;
+  endfunction
+  
+endclass
+
+module demo;
+  
+  initial begin
+    //Method 1
+    counter c = new(); 
+    // 'c' is a handle (reference)
+    // new() creates the object and assigns it to handle
+
+    //Method 2
+    // counter c;   //  Only declares a handle 
+    // c = new();   //  Creates object and assigns to handle
+  c.max = 6;
+  c.count =0;
+  repeat(10) begin
+    c.tick();
+    $display("count = %0d",c.count);
+  end
+  end
+  
+endmodule
+
+/*
+# KERNEL: count = 1
+# KERNEL: count = 2
+# KERNEL: count = 3
+# KERNEL: count = 4
+# KERNEL: count = 5
+# KERNEL: count = 6
+# KERNEL: count = 0
+# KERNEL: count = 1
+# KERNEL: count = 2
+# KERNEL: count = 3 
+*/
