fa38c987f089cb68f4419cd5490c7a1372ca3cee
Vighnesh E S <Vighnesh1510@gmail.com>
2026-05-15 22:12:54 +0530

    Create 1_fork_join_none.sv

---
+++ 11_THREADS/4_DISABLE_KEYWORD/1_fork_join_none.sv
@@ -0,0 +1,36 @@
+
+module fork_join_none;
+  
+  initial
+    begin
+      $display("time = %0t ,Fork block begins",$time);
+      
+      fork 
+        #1 $display("time =%0t , task begins",$time);
+        begin
+          #2 $display("time = %0t ,task_1 begins",$time);
+          #10 $display("time = %0t ,task_1 ends",$time);
+        end
+        
+        begin
+          #3 $display("time = %0t ,task_2 begins",$time);
+          #1 $display("time = %0t ,task_2 ends",$time);
+        end
+        begin
+          #7 $display("time = %0t , task_3 begins",$time);
+          #3 $display("time = %0t , task_3 ends",$time);
+        end
+      join_none
+      #1 $display("time = %0t , out of fork block",$time);
+      disable fork;
+      #2 $display("time = %0t , Join block begins",$time);
+    end
+endmodule
+
+        
+/*
+OUTPUT
+time = 0 ,Fork block begins
+time = 1 , out of fork block
+time = 3 , Join block begins
+*/
